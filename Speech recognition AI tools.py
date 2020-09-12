e9482703742af06d300e046ed073b453e1505aab
BattinaHimaja <68233251+BattinaHimaja@users.noreply.github.com>
2020-09-12 13:10:57 +0530

    Speech recognition.py

--- Speech recognition AI tools.py
+++ Speech recognition AI tools.py
@@ -1,11 +1,17 @@
-import speech_recognition as sr
 
+import speech_recognition as sr
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Speak Anything :")
-    audio = r.record(source)
-    try:
+audio_source=int(input("which source do you prefer ? \n Enter 1 if your choice is file \n Enter 2 if your choice is microphone \n "))
+if(audio_source==1):
+        filename=input("enter your file name")
+        with sr.AudioFile(filename) as source:
+        	audio=r.record(source)
+elif(audio_source==2):
+    with sr.Microphone() as source:
+    	print("Speak Anything :")
+    	audio = r.record(source)
+try:
         text = r.recognize_google(audio)
         print("You said : {}".format(text))
-    except:
-        print("Sorry could not recognize what you said")
+except:
+	print("Sorry could not recognize what you said")
